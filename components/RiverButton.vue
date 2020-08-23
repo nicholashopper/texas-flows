4177135a171e8033ea4bf9be38c188a83d00a982
nicholashopper <nicholashopper@iheartmedia.com>
2020-08-22 19:46:09 -0500

    Add basic button style

---
+++ components/RiverButton.vue
@@ -0,0 +1,54 @@
+<template>
+  <span id="RiverButton" class="box" v-if="river.name">
+    <div class="chart">
+      <div class="info">
+        <b>
+          <h1>{{ river.rating }}</h1>
+          <h5>{{ river.name }}</h5>
+        </b>
+      </div>
+    </div>
+  </span>
+</template>
+
+<script>
+export default {
+  name: "RiverButton",
+  props: ['river']
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+span.box {
+  display: inline-block;
+  width: 160px;
+  height: 160px;
+  padding: 16px;   
+  margin: 16px;
+  background-color: white; 
+  box-shadow: 0 0 10px gray;
+  text-align: center;
+  overflow: hidden;
+}
+
+div.chart {
+  width: 128px;
+  height: 128px;
+	padding: 8px;
+	text-align: center;
+  display: inline-block;
+  background-color: #888;
+  color: white; 
+}
+
+div.info {
+  margin: auto;
+  padding-top: 24px;
+}
+
+span.box:hover {
+  background-color: #fafafa;
+  box-shadow: 0 0 8px gray;
+}
+</style>
--- components/TotalRiverReport.vue
+++ components/TotalRiverReport.vue
@@ -1,11 +1,11 @@
 <template>
   <div id="TotalRiverReport">
     <div v-if="isLoaded">
-      <SingleRiverReport
+      <RiverButton
         v-for="river in rivers"
         v-bind:key="river.name"
         v-bind:river="river"
-      ></SingleRiverReport>
+      ></RiverButton>
     </div>
   </div>
 </template>
@@ -25,6 +25,7 @@ export default {
   async mounted() {
     const rivers = await import("../rivers.json");
     this.rivers = rivers;
+    console.log(this.rivers);
     this.isLoaded = true;
   }
 };
@@ -32,6 +33,9 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
+div {
+  overflow: hidden;
+}
 h3 {
   margin: 40px 0 0;
 }
